3f9238116a81cee199360bd9e5ba24bfda3e557e
roudoukou <xg-sco@qq.com>
2023-06-25 20:38:49 +0800

    整合swagger接口文档到api

--- xuecheng-plus-base/src/main/java/com/xuecheng/base/model/PageParams.java
+++ xuecheng-plus-base/src/main/java/com/xuecheng/base/model/PageParams.java
@@ -1,5 +1,6 @@
 package com.xuecheng.base.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,8 +15,10 @@ import lombok.ToString;
 @NoArgsConstructor
 public class PageParams {
     // 当前页码
+    @ApiModelProperty("页面")
     private Long pageNo = 1L;
 
     // 每页记录数默认值
+    @ApiModelProperty("每页记录数")
     private Long pageSize = 10L;
 }
