e919dc1eb9d123b639807847588af0fe74b12f0c
roudoukou <xg-sco@qq.com>
2023-06-25 20:01:39 +0800

    接口定义

---
+++ xuecheng-plus-base/src/main/java/com/xuecheng/base/model/PageResult.java
@@ -0,0 +1,33 @@
+package com.xuecheng.base.model;
+
+import lombok.Data;
+import lombok.ToString;
+
+import java.util.List;
+
+/**
+ * 分页查询结果模型类
+ * @param <T>
+ */
+@Data
+@ToString
+public class PageResult<T> {
+    // 数据列表
+    private List<T> items;
+
+    // 总记录数
+    private long counts;
+
+    // 当前页面
+    private long page;
+
+    // 每页记录数
+    private long pageSize;
+
+    public PageResult(List<T> items, long counts, long page, long pageSize) {
+        this.items = items;
+        this.counts = counts;
+        this.page = page;
+        this.pageSize = pageSize;
+    }
+}
