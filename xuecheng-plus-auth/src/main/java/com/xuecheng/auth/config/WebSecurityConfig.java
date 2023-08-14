fd7fdedb44cb4845f10b3257d9e89300d6773e7d
肉豆蔻吖 <1023876294@qq.com>
2023-08-14 18:24:30 +0800

    用户认证-统一认证入口

--- xuecheng-plus-auth/src/main/java/com/xuecheng/auth/config/WebSecurityConfig.java
+++ xuecheng-plus-auth/src/main/java/com/xuecheng/auth/config/WebSecurityConfig.java
@@ -1,7 +1,9 @@
 package com.xuecheng.auth.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -27,6 +29,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
+
+    @Autowired
+    DaoAuthenticationProviderCustom daoAuthenticationProviderCustom;
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProviderCustom);
+    }
+
 
     //配置用户信息服务
     /*@Bean
--- xuecheng-plus-auth/src/main/java/com/xuecheng/ucenter/service/impl/UserServiceImpl.java
+++ xuecheng-plus-auth/src/main/java/com/xuecheng/ucenter/service/impl/UserServiceImpl.java
@@ -3,19 +3,27 @@ package com.xuecheng.ucenter.service.impl;
 import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.xuecheng.ucenter.mapper.XcUserMapper;
+import com.xuecheng.ucenter.model.dto.AuthParamsDto;
 import com.xuecheng.ucenter.model.po.XcUser;
+import com.xuecheng.ucenter.service.AuthService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class UserServiceImpl implements UserDetailsService {
 
     @Autowired
     XcUserMapper xcUserMapper;
+
+    @Autowired
+    ApplicationContext applicationContext;
 
     /**
      * @description 根据账号查询用户信息
@@ -26,8 +34,25 @@ public class UserServiceImpl implements UserDetailsService {
      */
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        // 账号
-        String username = s;
+        AuthParamsDto authParamsDto = null;
+        try {
+            //将认证参数转为AuthParamsDto类型
+            authParamsDto = JSON.parseObject(s, AuthParamsDto.class);
+        } catch (Exception e) {
+            log.info("认证请求不符合项目要求:{}",s);
+            throw new RuntimeException("认证请求数据格式不对");
+        }
+
+        String authType = authParamsDto.getAuthType();
+        AuthService authService
+                = applicationContext.getBean(authType + "_authservice", AuthService.class);
+
+        //开始认证
+        authService.execute(authParamsDto);
+
+        //账号
+        String username = authParamsDto.getUsername();
+
         // 根据username账号查询数据库
         XcUser xcUser = xcUserMapper.selectOne(new LambdaQueryWrapper<XcUser>().eq(XcUser::getUsername, username));
         // 查询到用户不存在, 要返回null即可, Spring Security框架抛出异常用户不存在
