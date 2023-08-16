9448ad06e4713138f0bc4ac36df63c4f105a346a
肉豆蔻吖 <1023876294@qq.com>
2023-08-16 21:56:09 +0800

    授权从数据库查询权限

--- xuecheng-plus-auth/src/main/java/com/xuecheng/ucenter/service/impl/UserServiceImpl.java
+++ xuecheng-plus-auth/src/main/java/com/xuecheng/ucenter/service/impl/UserServiceImpl.java
@@ -2,9 +2,11 @@ package com.xuecheng.ucenter.service.impl;
 
 import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.xuecheng.ucenter.mapper.XcMenuMapper;
 import com.xuecheng.ucenter.mapper.XcUserMapper;
 import com.xuecheng.ucenter.model.dto.AuthParamsDto;
 import com.xuecheng.ucenter.model.dto.XcUserExt;
+import com.xuecheng.ucenter.model.po.XcMenu;
 import com.xuecheng.ucenter.model.po.XcUser;
 import com.xuecheng.ucenter.service.AuthService;
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +18,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Slf4j
 @Service
 public class UserServiceImpl implements UserDetailsService {
@@ -26,10 +31,13 @@ public class UserServiceImpl implements UserDetailsService {
     @Autowired
     ApplicationContext applicationContext;
 
+    @Autowired
+    XcMenuMapper xcMenuMapper;
+
     /**
-     * @description 查询用户信息组成用户身份信息
-     * @param s  AuthParamsDto类型的json数据
+     * @param s AuthParamsDto类型的json数据
      * @return org.springframework.security.core.userdetails.UserDetails
+     * @description 查询用户信息组成用户身份信息
      * @author Mr.M
      * @date 2022/9/28 18:30
      */
@@ -41,13 +49,13 @@ public class UserServiceImpl implements UserDetailsService {
             //将认证参数转为AuthParamsDto类型
             authParamsDto = JSON.parseObject(s, AuthParamsDto.class);
         } catch (Exception e) {
-            log.info("认证请求不符合项目要求:{}",s);
+            log.info("认证请求不符合项目要求:{}", s);
             throw new RuntimeException("认证请求数据格式不对");
         }
 
         //认证方法
         String authType = authParamsDto.getAuthType();
-        AuthService authService =  applicationContext.getBean(authType + "_authservice",AuthService.class);
+        AuthService authService = applicationContext.getBean(authType + "_authservice", AuthService.class);
         XcUserExt user = authService.execute(authParamsDto);
 
         return getUserPrincipal(user);
@@ -55,23 +63,35 @@ public class UserServiceImpl implements UserDetailsService {
 
 
     /**
-     * @description 查询用户信息
-     * @param user  用户id，主键
+     * @param user 用户id，主键
      * @return com.xuecheng.ucenter.model.po.XcUser 用户信息
+     * @description 查询用户信息
      * @author Mr.M
      * @date 2022/9/29 12:19
      */
-    public UserDetails getUserPrincipal(XcUserExt user){
-        //用户权限,如果不加报Cannot pass a null GrantedAuthority collection
-        String[] authorities = {"p1"};
+    //查询用户身份
+    public UserDetails getUserPrincipal(XcUserExt user) {
         String password = user.getPassword();
+        //查询用户权限
+        List<XcMenu> xcMenus = xcMenuMapper.selectPermissionByUserId(user.getId());
+        List<String> permissions = new ArrayList<>();
+        if (xcMenus.size() <= 0) {
+            //用户权限,如果不加则报Cannot pass a null GrantedAuthority collection
+            permissions.add("test");
+        } else {
+            xcMenus.forEach(menu -> {
+                permissions.add(menu.getCode());
+            });
+        }
+        //将用户权限放在XcUserExt中
+        user.setPermissions(permissions);
+
         //为了安全在令牌中不放密码
         user.setPassword(null);
         //将user对象转json
         String userString = JSON.toJSONString(user);
-        //创建UserDetails对象
-        UserDetails userDetails = User.withUsername(userString).password(password ).authorities(authorities).build();
+        String[] authorities = permissions.toArray(new String[0]);
+        UserDetails userDetails = User.withUsername(userString).password(password).authorities(authorities).build();
         return userDetails;
     }
-
 }
